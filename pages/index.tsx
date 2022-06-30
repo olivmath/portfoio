eab053488f1807cb041c9a8067f6e59de317229b
Lucas Oliveira <olivmath@protonmail.com>
2022-06-30 10:54:50 -0300

    update: home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,27 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+import tw from "tailwind-styled-components"
+
+const Container = tw.div`
+  m-8
+`
+const TitleText = tw.h1`
+  text-xl
+`
+const SubTitleText = tw.h2`
+  text-lg
+`
+const ImageItem = tw.li`
+  grid
+  justify-items-center
+`
+const Grid = tw.ul`
+  grid
+  grid-cols-2
+`
+const TextList = tw.ul``
+const Item = tw.li``
 
 const Home: NextPage = () => {
   return (
@@ -7,12 +29,28 @@ const Home: NextPage = () => {
       <Head>
         <title>Home | olivmath portfolio</title>
       </Head>
-      <h1 className='text-xl ml-8'>
-        Lucas Oliveira
-      </h1>
-      <h2 className='text-lg ml-8'>
-        Senior Blockchain Engineer
-      </h2>
+      <Container>
+        <Grid>
+          <Item>
+            <TextList>
+              <TitleText>
+                Lucas Oliveira
+              </TitleText>
+              <SubTitleText>
+                Senior Blockchain Engineer
+              </SubTitleText>
+            </TextList>
+          </Item>
+          <ImageItem>
+            <Image
+              src="/images/profile.png"
+              height={144}
+              width={144}
+              alt="Your Name"
+            />
+          </ImageItem>
+        </Grid>
+      </Container>
     </>
   )
 }
