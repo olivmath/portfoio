81b242f964a7d8421e6a9204f220b7919022f83f
Lucas Oliveira <olivmath@protonmail.com>
2022-06-29 23:17:08 -0300

    update: social link

--- components/Navigation/SocialLink/SocialLink.component.tsx
+++ components/Navigation/SocialLink/SocialLink.component.tsx
@@ -1,1 +1,17 @@
-export {}
+import Link from "next/link"
+
+export interface Props {
+  url: string,
+  children: string
+}
+
+const SocialLink = ({url, children}: Props) => {
+  return (
+    <Link href={url}>
+      <a>{children}</a>
+    </Link>
+  )
+}
+
+
+export default SocialLink
